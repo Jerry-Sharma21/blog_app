b253f6876403ab33200c0e78589a46fb3b15817e
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-01-30 12:15:43 +0530

    test api routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+
+import userRoutes from './routes/user.route';
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI as string)
   .then(() => {
     console.log('MongoDB is connected');
   })
@@ -18,3 +20,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use('/api/user', userRoutes);
