b4eb2d21bd30065dcbaf71253579d8f3c6a9b524
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-01-30 11:26:01 +0530

    create and run the server

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -44,7 +44,9 @@ const Header: React.FC = () => {
         </Button>
 
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+          <Button gradientDuoTone='purpleToBlue' outline>
+            Sign In
+          </Button>
         </Link>
 
         <Navbar.Toggle />
